f1853cb5a30a05dc1e976bc91a55512cfe759f1b
Daniel Ancuta <daniel.ancuta@3ss.tv>
2020-05-24 12:09:21 +0300

    module 4-1

--- src/index.js
+++ src/index.js
@@ -6,6 +6,3 @@ export function fetchMovies() {
     const resolveFunction = () => movies;
     return fetchWithTimeout(1000).then(resolveFunction);
 }
-
-const moviePromise = fetchMovies();
-moviePromise.then(results => {console.log(results)});
