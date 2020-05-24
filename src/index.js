6001fd5cafcd11bd4747168003945675ae85b70e
Daniel Ancuta <daniel.ancuta@3ss.tv>
2020-05-24 12:23:21 +0300

    module 4-7

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import {fetchWithTimeout, fetchMovies,fetchBooks} from "./services";
 
 const movies = require('./data/movies.json');
 
+function getBooksAndMovies() {
+    return Promise.all(([fetchMovies(),fetchBooks()])).then(([books, movies]) => ({
+        books,
+        movies
+    })).catch(error => console.log("Error fetching books and movies", error))
+}
